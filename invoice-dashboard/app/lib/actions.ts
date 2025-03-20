75315f7b84fb0cc69ff1fbee43fde0d6daf8f701
lmea0005 <lmea0005@student.monash.edu>
2025-03-20 20:17:31 +1100

    Handle invoice creation

---
+++ invoice-dashboard/app/lib/actions.ts
@@ -0,0 +1,44 @@
+'use server';
+
+import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+/*
+zod schema to validate invoice data is in these formats
+*/
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    // 'coerce' changes the amount from a string to a number
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+    // push invoice entry into postgres
+    await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+
+    // clear the cache so the invoice table gets updated properly with fresh data (because we just pushed data)
+    revalidatePath('/dashboard/invoices');
+
+    // now navigate back to the invoice table
+    redirect('/dashboard/invoices');
+}
